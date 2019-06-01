e116dbead4cd5d4a1b055ca8c627513f7571fa2c
Archi39 <ey.samarin@gmail.com>
2019-06-01 17:48:37 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    predictAge(65, 60, 75, 55, 60, 63, 64, 45)
+}
+
+fun predictAge(vararg ages: Int): Int{
+    var i = 0 
+        ages.forEach {
+        i+= it*it
+    }
+    return Math.floor(Math.sqrt(i.toDouble())/2).toInt()
+}
